c81b90646e3fa5fc49eec9b3563f120783ee5abd
Anirudh Ananthasubramaniam <anirudhananth99@gmail.com>
2024-02-13 00:40:44 -0500

    Added vertical enemy spawn

--- Assets/Scripts/Singletons/Spawner.cs
+++ Assets/Scripts/Singletons/Spawner.cs
@@ -19,6 +19,7 @@ public class Spawner : MonoBehaviour
         cameraHeight = Camera.main.orthographicSize * 2f;
         cameraWidth = cameraHeight * Camera.main.aspect;
         StartCoroutine(SpawnEnemy());
+        StartCoroutine(SpawnTopDownEnemy());
     }
 
     private IEnumerator SpawnEnemy() {
@@ -44,6 +45,22 @@ public class Spawner : MonoBehaviour
         }
     }
 
+    private IEnumerator SpawnTopDownEnemy() {
+        while(canSpawn) {
+            yield return new WaitForSeconds(Random.Range(2, 4));
+
+            float multX = Mathf.Pow(-1, Random.Range(0, 2));
+            x = Camera.main.transform.position.x + multX * Random.Range(0, cameraWidth / 2f);
+            y = Camera.main.transform.position.y + cameraHeight / 2f;
+            GameObject enemy = Instantiate(enemyPrefab, new Vector2(x, y), Quaternion.identity);
+
+            Vector2 direction = Vector2.down + new Vector2(Random.Range(0, 2) * multX, Random.Range(0, 2) * -1f);
+            direction.Normalize();
+            enemy.GetComponent<Rigidbody2D>().AddForce(direction * 5f, ForceMode2D.Impulse);
+            spawnedEnemies.Add(enemy);
+        }
+    }
+
     // Update is called once per frame
     void Update() {
         if(spawnedEnemies.Count > 0) CheckAndDestroyEnemies();
@@ -60,6 +77,11 @@ public class Spawner : MonoBehaviour
             }
 
             if(direction.x < 0 && CrossedLeftSide(enemy)) {
+                enemiesToDestroy.Add(enemy);
+                Destroy(enemy);
+            }
+
+            if(direction.y < 0 && CrossedBottomSide(enemy)) {
                 enemiesToDestroy.Add(enemy);
                 Destroy(enemy);
             }
@@ -87,6 +109,14 @@ public class Spawner : MonoBehaviour
     bool CrossedLeftSide(GameObject go) {
         SpriteRenderer renderer = go.GetComponentInChildren<SpriteRenderer>();
         if(go.transform.position.x + renderer.bounds.size.x < Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x) {
+            return true;
+        }
+        return false;
+    }
+
+    bool CrossedBottomSide(GameObject go) {
+        SpriteRenderer renderer = go.GetComponentInChildren<SpriteRenderer>();
+        if(go.transform.position.y + renderer.bounds.size.y < Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y) {
             return true;
         }
         return false;
